22b88be689b341f560044dcc92335abed8b0aff7
Paramet Kongjaroen <prame@MacBook-Air-khxng-Paramet.local>
2024-08-01 11:28:27 +0700

    feat: integrate sqlite

--- apps/backend/src/holder/holder.module.ts
+++ apps/backend/src/holder/holder.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HolderController } from './holder.controller';
 import { HolderService } from './holder.service';
 import { UserService } from './user.service';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   controllers: [HolderController],
-  providers: [HolderService, UserService],
+  providers: [HolderService, UserService, PrismaService],
 })
 export class HolderModule {}
--- apps/backend/src/holder/user.service.ts
+++ apps/backend/src/holder/user.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 import { SiweMessage } from 'siwe';
 
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) {}
+
   async signIn(message: string, signature: string, domain: string) {
     const siwe = new SiweMessage(JSON.parse(message || '{}'));
 
@@ -12,7 +15,22 @@ export class UserService {
     });
 
     if (result.success) {
-      return siwe.address;
+      const holder = await this.prisma.holder.findUnique({
+        where: { id: siwe.address },
+      });
+
+      if (holder) {
+        return holder.address;
+      }
+
+      const newHolder = await this.prisma.holder.create({
+        data: {
+          id: siwe.address,
+          address: siwe.address,
+        },
+      });
+
+      return newHolder.address;
     }
 
     return null;
