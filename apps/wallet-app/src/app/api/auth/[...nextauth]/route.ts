0ba2245c8133f212fa36421a8009f80ec043cf51
Paramet Kongjaroen <prame@MacBook-Air-khxng-Paramet.local>
2024-08-08 14:38:06 +0700

    fix: build next-auth

--- apps/wallet-app/src/app/api/auth/[...nextauth]/route.ts
+++ apps/wallet-app/src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import { NextApiRequest, NextApiResponse } from "next";
 import { authOptions } from "../options";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: any, res: any) {
   const url = new URL(req.url!);
   const isDefaultSigninPage = url.pathname === "/api/auth/signin";
 
